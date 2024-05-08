de50490796c0e8ab02dc2d281ed85eb3c9b1004f
rosadevera <dever014@newschool.edu>
2024-05-08 17:13:08 -0400

    may 8 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Get the audio element
   const audioElement = document.getElementById('audio');
 
-  audioElement.volume = 0.4;
+  audioElement.volume = 0.5;
 
   // Function to define the folder based on the current hour
   function getCurrentHourFolder() {
@@ -77,17 +77,16 @@ document.addEventListener('DOMContentLoaded', function() {
   setInterval(updateDateTime, 1000); // Every second 1000 milliseconds)
 
   // Function to set background gradient
-function setBackgroundGradient() {
-  let now = new Date();
-  let hour = now.getHours();
-  let gradientColors;
-  let skyElement = document.querySelector('.sky');
-  let currentGradient = skyElement.style.backgroundImage;
+  function setBackgroundGradient() {
+    let now = new Date();
+    let hour = now.getHours();
+    let gradientColors;
+    let skyElement = document.querySelector('.sky');
 
   if (hour >= 5 && hour < 6) {
     // Sunrise gradient
     gradientColors = "#4e6799 0%, #6096b3 50%, #e0c4af 100%";
-    skyElement.style.filter = 'blur(2px) opacity(50%)'
+    skyElement.style.filter = 'blur(2px) opacity(50%)';
     document.querySelector('.rainbow').style.visibility = 'hidden';
   } else if (hour >= 7 && hour < 17) {
     // Morning/afternoon gradient
@@ -97,35 +96,21 @@ function setBackgroundGradient() {
   } else if (hour >= 17 && hour < 19) {
     // Sunset gradient
     gradientColors = "#6096b3 0%, #4e6799 50%, #766fa1 100%";
-    skyElement.style.filter = 'blur(2px) opacity(50%)'
+    skyElement.style.filter = 'blur(2px) opacity(50%)';
     document.querySelector('.rainbow').style.visibility = 'hidden';
   } else {
     // Night gradient
     gradientColors = "#131421 0%, #111724 50%, #070708 100%";
-    skyElement.style.filter = 'blur(5px) opacity(10%)'
+    skyElement.style.filter = 'blur(5px) opacity(10%)';
     document.querySelector('.rainbow').style.visibility = 'hidden';
   }
 
   // Apply gradient colors with transition effect
   skyElement.style.transition = 'background-image 1s ease-in-out';
-  let newGradient = `radial-gradient(${gradientColors})`;
-  
-  // Update the background gradient gradually
-  let steps = 100; // Adjust the number of steps for smoother transition
-  let currentStep = 0;
-  let interval = setInterval(function() {
-    currentStep++;
-    let percentage = (currentStep / steps) * 100;
-    let interpolatedGradient = `radial-gradient(${gradientColors}) ${percentage}% ${percentage}%`;
-    document.body.style.backgroundImage = `${currentGradient}, ${interpolatedGradient}`;
-    if (currentStep >= steps) {
-      clearInterval(interval);
-      document.body.style.backgroundImage = newGradient;
-    }
-  }, 50); // Adjust the interval for smoother transition
-}
+  document.body.style.backgroundImage = `radial-gradient(${gradientColors})`;
+}  
 
-  setBackgroundGradient();
+setBackgroundGradient();
 
   // Get the input field
   const taskInput = document.querySelector('.task-input');
